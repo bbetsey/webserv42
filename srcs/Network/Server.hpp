a30c754c93a175c06fe79163fc4e2a987d2e2d56
bbetsey <bbetsey@student.21-school.ru>
2022-01-17 20:02:41 +0300

    feat(Server & Client): made .hpp files; first day, head is spinning

---
+++ srcs/Network/Server.hpp
@@ -0,0 +1,14 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+#include <string>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <sys/select.h>
+
+
+
+#endif
