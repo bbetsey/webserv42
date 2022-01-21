064eb6858faf151ebc82ef981f55e07ad09fce7a
bbetsey <bbetsey@student.21-school.ru>
2022-01-21 17:56:41 +0300

    feat(Network): we can read

--- srcs/Network/Network.cpp
+++ srcs/Network/Network.cpp
@@ -28,33 +28,81 @@ void	Network::watch_loop( int kq, struct kevent *kset, int len ) {
 		for ( int i = 0; i < new_event; ++i ) {
 			int event_fd = events[i].ident;
 			if ( events[i].flags & EV_EOF ) {
-				std::cout << "Disconnect" << std::endl;
+				std::cout << "Disconnect request" << std::endl;
 				EV_SET( &events[i], event_fd, EVFILT_READ, EV_DELETE, 0, 0, 0 );
 				if ( kevent( kq, &events[i], 1, NULL, 0, NULL ) == -1 ) {
 					std::cout << "Network: kevent delete error" << std::endl;
 					break;
 				}
+				std::cout << "Disconnected" << std::endl;
 				close( event_fd );
 			}
-			else if ( is_listen_socket( kset, events[i].ident ) ) {
+			else if ( is_listen_socket( kset, events[i].ident, len ) ) {
+				std::cout << "New connection request" << std::endl;
 				int client_fd = accept( events[i].ident, ( struct sockaddr * )&addr, &socklen );
+				
 				struct kevent event_set;
+				t_udata	new_data[1];
+				new_data->is_send = 0;
+				new_data->listen_socket = events[i].ident;
+				event_set.udata = new_data;
 				EV_SET( &event_set, client_fd, EVFILT_READ, EV_ADD, 0, 0, 0 );
 				if ( kevent( kq, &event_set, 1, NULL, 0, NULL ) == -1 ) {
 					std::cout << "Network: kevent add new client" << std::endl;
 					break;
 				}
+
+				std::cout << "New connection accepted" << std::endl;
 			}
 			else if ( events[i].filter == EVFILT_READ ) {
-				recv_msg( events[i].ident );
-				
+				std::cout << "In read filter" << std::endl;
+				recv_msg( events[i] );
+				struct kevent event_set;
+				EV_SET( &event_set, event_fd, EVFILT_WRITE, EV_ADD, 0, 0, 0 );
+				if ( kevent( kq, &event_set, 1, NULL, 0, NULL ) == -1 ) {
+					std::cout << "Network: kevent add new client" << std::endl;
+					break;
+				}
 			}
 			else if ( events[i].filter == EVFILT_WRITE ) {
-				send_msg( events[i].ident );
+				std::cout << "Check udata flag" << std::endl;
+				t_udata *data = ( t_udata * )events[i].udata;
+
+				if ( !data->is_send ) {
+					std::cout << "In write filter" << std::endl;
+					send_msg( events[i] );
+					data->is_send = 1;
+				}
 			}
 		}
 	}
 
+}
+
+int	Network::is_listen_socket( struct kevent *kset, int fd, int len ) {
+	for ( int i = 0; i < len; ++i, ++kset )
+		if ( kset->ident == ( unsigned long )fd ) return 1;
+	return 0;
+}
+
+void	Network::recv_msg( struct kevent &event ) {
+	char	buf[ BUFFER_READ ];
+	size_t	read_bytes;
+
+	read_bytes = recv( event.ident, buf, sizeof( buf ), 0 );
+	
+	std::cout << "\n========== REQUEST MSG ==========" << std::endl;
+	if ( read_bytes > 0 ) {
+		buf[ read_bytes ] = '\0';
+		std::cout << buf << std::endl;
+	}
+	std::cout << "\n========== END MSG ==========" << std::endl;
+}
+
+void	Network::send_msg( struct kevent &event ) {
+	std::string msg = "Hello World!";
+
+	send( event.ident, msg.c_str(), msg.length(), 0 );
 }
 
 
